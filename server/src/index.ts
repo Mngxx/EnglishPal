1b4bebfdf2e200b662bd1a4402430d772f27d623
Ron Lara <lararon2428@gmail.com>
2026-07-23 14:52:36 +0800

    feat: add chat router post in root express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import chatRouter from './routes/chat.ts'
 
 dotenv.config()
 
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 3000
 
 app.use(cors({ origin: 'http://localhost:5173'}))
 app.use(express.json())
+
+app.use('/chat', chatRouter)
 
 app.get('/health', (req, res) =>{
     res.json({ status: 'ok' })
