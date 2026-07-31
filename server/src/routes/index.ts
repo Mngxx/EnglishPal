ccead2a44513acc804fd5dac5e88296d1912a82e
Ron Lara <lararon2428@gmail.com>
2026-07-31 17:29:32 +0800

    refactor: use barrel file approach for fetching router on /routes directory

---
+++ server/src/routes/index.ts
@@ -0,0 +1,6 @@
+import chatRouter from "./chat";
+import feedbackRouter from "./feedback";
+import sessionRouter from "./sessions";
+import validateKeyRouter from "./validateKey";
+
+export { chatRouter, feedbackRouter, sessionRouter, validateKeyRouter };
