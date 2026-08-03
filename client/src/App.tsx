14a6b3f7fabead68dd9a525537cfaa7907b9357d
Ron Lara <lararon2428@gmail.com>
2026-08-03 18:46:40 +0800

    feat: added routes for the login and signup pages on the app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Dashboard } from "./pages/Dashboard";
 import { History } from "./pages/History";
+import { Login } from "./pages/Login";
 import { Session } from "./pages/Session";
 import { SessionDetail } from "./pages/SessionDetail";
+import { Signup } from "./pages/Signup";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				{/* Define routes mapping paths to components */}
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
 				<Route path="/" element={<Session />} />
 				<Route path="/history" element={<History />} />
 				<Route path="/history/:id" element={<SessionDetail />} />
